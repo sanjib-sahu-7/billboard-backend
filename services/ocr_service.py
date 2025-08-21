a8a9d31c3cc8adb9fd60a59328e3baef2644b1e2
sanjib-sahu-7 <sahusanjib459@gmail.com>
2025-08-21 15:30:49 +0530

    Initial commit - Billboard Backend

--- services/ocr_service.py
+++ services/ocr_service.py
@@ -1,22 +1,46 @@
+
 import pytesseract
 from config import BANNED_KEYWORDS
-
-# Set Tesseract executable path (Windows)
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+import cv2
 
 def check_text_violation(image_path, bbox):
     """
     Extract text from ROI and check against banned keywords.
     Returns list of violation reasons.
     """
-    import cv2
     x, y, w, h = bbox
     img = cv2.imread(image_path)
     roi = img[y:y+h, x:x+w]
-    text = pytesseract.image_to_string(roi)
-    reasons = []
 
+    # Run Tesseract (Render/Linux will use system install)
+    text = pytesseract.image_to_string(roi)
+
+    reasons = []
     for word in BANNED_KEYWORDS:
         if word.lower() in text.lower():
             reasons.append(f"Content Violation: {word}")
     return reasons
+
+
+# import pytesseract
+# from config import BANNED_KEYWORDS
+
+# # Set Tesseract executable path (Windows)
+# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# def check_text_violation(image_path, bbox):
+#     """
+#     Extract text from ROI and check against banned keywords.
+#     Returns list of violation reasons.
+#     """
+#     import cv2
+#     x, y, w, h = bbox
+#     img = cv2.imread(image_path)
+#     roi = img[y:y+h, x:x+w]
+#     text = pytesseract.image_to_string(roi)
+#     reasons = []
+
+#     for word in BANNED_KEYWORDS:
+#         if word.lower() in text.lower():
+#             reasons.append(f"Content Violation: {word}")
+#     return reasons
