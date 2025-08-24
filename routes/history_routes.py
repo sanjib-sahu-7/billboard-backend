651847847cd75f901dcc3e6a81e0f88e6b2c7db8
sanjib-sahu-7 <sahusanjib459@gmail.com>
2025-08-25 02:35:36 +0530

    history route added

--- routes/history_routes.py
+++ routes/history_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME, COLLECTION_NAME
+from config import MONGO_URI, DB_NAME
+from bson.objectid import ObjectId
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
@@ -9,10 +10,22 @@ reports_col = db["user_reports"]
 
 history_bp = Blueprint("history", __name__)
 
+
 @history_bp.route("/", methods=["GET"])
 @jwt_required()
 def get_user_history():
     current_user = get_jwt_identity()
-    # Query MongoDB for reports where reported_by equals current user
-    records = list(reports_col.find({"reported_by": current_user}, {"_id": 0}))
+    
+    records_cursor = reports_col.find({"reported_by": current_user})
+    records = []
+    for record in records_cursor:
+        # Convert _id and any ObjectId fields to strings
+        record['_id'] = str(record.get('_id', ''))
+        
+        # Convert image_file_id ObjectId to string if present
+        if 'image_file_id' in record and isinstance(record['image_file_id'], ObjectId):
+            record['image_file_id'] = str(record['image_file_id'])
+        
+        records.append(record)
+    
     return jsonify(records)
