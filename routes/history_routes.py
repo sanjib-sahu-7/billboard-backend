3ab9c0c5039091485c91a4a53fce26fd4484d93c
sanjib-sahu-7 <sahusanjib459@gmail.com>
2025-08-25 01:35:49 +0530

    history route added

--- routes/history_routes.py
+++ routes/history_routes.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from pymongo import MongoClient
 from config import MONGO_URI, DB_NAME, COLLECTION_NAME
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
-images_col = db[COLLECTION_NAME]
+reports_col = db["user_reports"] 
 
 history_bp = Blueprint("history", __name__)
 
 @history_bp.route("/", methods=["GET"])
-def get_history():
-    """
-    Return all records from MongoDB
-    """
-    records = list(images_col.find({}, {"_id": 0}))
+@jwt_required()
+def get_user_history():
+    current_user = get_jwt_identity()
+    # Query MongoDB for reports where reported_by equals current user
+    records = list(reports_col.find({"reported_by": current_user}, {"_id": 0}))
     return jsonify(records)
